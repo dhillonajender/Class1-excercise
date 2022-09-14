a862593732421cba13446368151f650802a91bd6
dhillonajender <97201745+dhillonajender@users.noreply.github.com>
2022-09-13 20:01:35 -0400

    Add files via upload

---
+++ Class1-Excercise.py
@@ -0,0 +1,10 @@
+input1 = int(input("Enter your first number="))
+input2 = int(input("Enter your second number="))
+add = input1 + input2
+sub = input1 - input2
+mul = input1 * input2
+power = input1 ** input2
+division = input1 / input2
+print("Add: "+str(add)+ " Sub: "+str(sub)+" Multiplication: "+str(mul)+" Power: "+str(power)+" Division: "+str(division))
+    
+
