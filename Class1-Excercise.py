ceed6a18dcd69e4e81a162ec661a0ea7e3431870
Gurpartap-Uottawa <113481587+Gurpartap-Uottawa@users.noreply.github.com>
2022-09-13 20:16:40 -0400

    Update Class1-Excercise.py
    changes for  third input

--- Class1-Excercise.py
+++ Class1-Excercise.py
@@ -6,5 +6,11 @@ mul = input1 * input2
 power = input1 ** input2
 division = input1 / input2
 print("Add: "+str(add)+ " Sub: "+str(sub)+" Multiplication: "+str(mul)+" Power: "+str(power)+" Division: "+str(division))
+input3 = int(input("Enter your third number="))
+add2 = add + input3
+sub2 = sub - input3
+mul2 = mul * input3
+power2 = power ** input3
+division2 = division / input3
+print("Add: "+str(add2)+ " Sub: "+str(sub2)+" Multiplication: "+str(mul2)+" Power: "+str(power2)+" Division: "+str(division2))
     
-
